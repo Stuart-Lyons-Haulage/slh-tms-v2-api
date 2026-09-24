0bfbcf1ac0774bc3fa45c70d16df25f33b8dfffe
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-24 20:58:49 +0100

    fix: register customer collection site migration

--- Services/SchemaMigrationRunner.cs
+++ Services/SchemaMigrationRunner.cs
@@ -222,7 +222,8 @@ public static class SchemaMigrationRunner
         "072_Rescue_Aldi_Atherstone_Morrisons_Sittingbourne_PreOrders.sql",
         "073_Rejected_Order_DoNotLearn_Guard.sql",
         "074_Local_Tms_Users.sql",
-        "075_Canonical_Identity_Uniqueness.sql"
+        "075_Canonical_Identity_Uniqueness.sql",
+        "076_Customer_Collection_Sites_Geofence_Links.sql"
     ];
 
     internal const string HistoryTableSql = """
