57b2af4af93da37e84b64f984fd3c823f3d7cb2b
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-24 13:18:42 +0100

    fix: fully prepare migration 033 additive columns

--- Services/SchemaMigrationRunner.cs
+++ Services/SchemaMigrationRunner.cs
@@ -48,10 +48,30 @@ public static class SchemaMigrationRunner
         END;
         """;
     internal const string IntakeMappingGovernancePreparationSql = """
-        IF OBJECT_ID(N'dbo.IntegrationMappings', N'U') IS NOT NULL
-           AND COL_LENGTH(N'dbo.IntegrationMappings', N'NormalizedExternalValue') IS NULL
+        IF OBJECT_ID(N'dbo.Sites', N'U') IS NOT NULL
+           AND COL_LENGTH(N'dbo.Sites', N'OperationalRegion') IS NULL
         BEGIN
-            ALTER TABLE dbo.IntegrationMappings ADD NormalizedExternalValue nvarchar(300) NULL;
+            ALTER TABLE dbo.Sites ADD OperationalRegion nvarchar(80) NULL;
+        END;
+
+        IF OBJECT_ID(N'dbo.IntegrationMappings', N'U') IS NOT NULL
+        BEGIN
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'MappingKind') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD MappingKind nvarchar(40) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'NormalizedExternalValue') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD NormalizedExternalValue nvarchar(300) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'SenderPattern') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD SenderPattern nvarchar(320) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'TemplateName') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD TemplateName nvarchar(120) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'TemplateVersion') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD TemplateVersion nvarchar(40) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'ConfidenceThreshold') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD ConfidenceThreshold decimal(5,4) NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'EffectiveFromUtc') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD EffectiveFromUtc datetimeoffset NULL;
+            IF COL_LENGTH(N'dbo.IntegrationMappings', N'EffectiveToUtc') IS NULL
+                ALTER TABLE dbo.IntegrationMappings ADD EffectiveToUtc datetimeoffset NULL;
         END;
         """;
     private const string IntakeMappingGovernanceMigration = "033_Intake_Mapping_Governance.sql";
